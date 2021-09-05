351a1727e34a6def12c9ff33925656fecfa53d3c
DaryaRu <89119484+DaryaRu@users.noreply.github.com>
2021-09-05 11:47:01 +0300

    user_id берется из контекста

--- API/resources/like.py
+++ API/resources/like.py
@@ -1,23 +1,22 @@
+from flask_jwt import jwt_required, current_identity
 from flask_restful import Resource, reqparse
 from models.like import LikeModel
 from models.user import UserModel
 from models.publication import PublicationModel
 
+
 class Like(Resource):
     parser = reqparse.RequestParser()
-    parser.add_argument("user_id", type=int, required=True)
     parser.add_argument("publication_id", type=int, required=True)
 
+    @jwt_required()
     def post(self):
         data = Like.parser.parse_args()
-        user_id = data['user_id']
+        user_id = current_identity.id
         publication_id = data['publication_id']
 
-        if UserModel.find_by_id(user_id) is None:
-            return {"message": "A user with that id does'n exists"}, 404
-
         if PublicationModel.find_by_id(publication_id) is None:
-            return {"message": "A publication with that id does'n exists"}, 404  
+            return {"message": "A publication with that id does'n exists"}, 404
 
         like_in_table = LikeModel.find_like(user_id, publication_id)
 
@@ -28,21 +27,19 @@ class Like(Resource):
         else:
             return {"message": "The like already exists"}, 400
 
+    @jwt_required()
     def delete(self):
         data = Like.parser.parse_args()
-        user_id = data['user_id']
+        user_id = current_identity.id
         publication_id = data['publication_id']
-
-        if UserModel.find_by_id(user_id) is None:
-            return {"message": "A user with that id does'n exists"}, 404
 
         if PublicationModel.find_by_id(publication_id) is None:
             return {"message": "A publication with that id does'n exists"}, 404
 
         like = LikeModel.find_like(user_id, publication_id)
-        
+
         if like:
             like.remove_like_from_db()
             return {"message": "Your like deleted"}, 200
         else:
-            return {"message": "The like doesn't exist"}, 404    
+            return {"message": "The like doesn't exist"}, 404
